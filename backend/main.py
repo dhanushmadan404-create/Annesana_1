879ba92a3dfbc86c4c2693672574003b27555756
Dhanush Kumar M <dhanushmadan404@gmail.com>
2026-01-06 13:37:15 +0530

    changed folder structure

--- backend/main.py
+++ backend/main.py
@@ -24,7 +24,7 @@ origins = [
 ]
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-FRONTEND_DIR = BASE_DIR
+FRONTEND_DIR = os.path.join(BASE_DIR, "frontend")
 
 app.mount("/static", StaticFiles(directory=FRONTEND_DIR), name="static")
 
